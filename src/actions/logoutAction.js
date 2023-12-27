c67e4cd9db4874e94e4e4ce878006e129c0b61a8
dipankarpaul2k <dipankarpaul2k@gmail.com>
2023-12-27 11:36:34 +0530

    minor logout changes

--- src/actions/logoutAction.js
+++ src/actions/logoutAction.js
@@ -4,9 +4,9 @@ import { toast } from "react-toastify";
 
 export default async function logoutAction() {
   deleteItem("userName");
-  console.log("UserName deleted.");
+  // console.log("UserName deleted.");
   deleteItem("todos");
-  console.log("Todos deleted.");
+  // console.log("Todos deleted.");
   toast.info("You've deleted your account.");
   return redirect("/");
 }
